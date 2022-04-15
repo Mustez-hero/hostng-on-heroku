61c95c966ed6ffedd41ed3364c7bd627240a135e
Mustez-hero <87631522+Mustez-hero@users.noreply.github.com>
2022-04-15 09:16:11 +0000

    update App\http\Controllers\HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\HomeController;
+use App\http\Controllers\HomeController;
 
 class HomeController extends Controller
 {
